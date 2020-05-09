659dd7ec70e933f2e35d26505d3599725e4f8385
Dennis <nndenninn@gmail.com>
2020-05-09 11:35:01 +0300

    Initial Commit

---
+++ Project5-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5-iOS13
+//
+//  Created by Dennis Nesanoff on 09.05.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
